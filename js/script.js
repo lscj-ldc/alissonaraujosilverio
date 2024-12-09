7898aa670f29242f595ad8e4b847dd6d589de132
Luís da Silveira Carneiro Júnior <lscj.ldc@gmail.com>
2024-12-09 14:31:42 -0300

    Atualização

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const resumeBtns = document.querySelectorAll('.resume-btn');
+resumeBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        resumeBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+    });
+});
