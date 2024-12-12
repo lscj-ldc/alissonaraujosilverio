6471c85a50fc646bd550ddc1d490971f2a037cb5
Luís da Silveira Carneiro Júnior <lscj.ldc@gmail.com>
2024-12-12 15:02:07 -0300

    Correção do código

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const resumeBtns = document.querySelectorAll('.resume-btn');
 resumeBtns.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
         const resumeDetails = document.querySelectorAll('.resume-detail');
-
         resumeBtns.forEach(btn => {
             btn.classList.remove('active');
         });
@@ -12,33 +11,4 @@ resumeBtns.forEach((btn, idx) => {
         });
         resumeDetails[idx].classList.add('active');
     });
-});
-const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
-const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
-let index = 0;
-const activePortfolio = () => {
-    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
-    imgSlide.style.transform = 'translateX(calc(${index * -100}% - ${index * 2}rem))';
-}
-arrowRight.addEventListener('click', () => {
-    if (index < 4) {
-        index++;
-        arrowLeft.classList.remove('disabled');
-    }
-    else {
-        index = 5;
-        arrowRight.classList.add('disabled');
-    }
-    activePortfolio();
-});
-arrowLeft.addEventListener('click', () => {
-    if (index > 1) {
-        index--;
-        arrowRight.classList.remove('disabled');
-    }
-    else {
-        index = 0;
-        arrowLeft.classList.add('disabled');
-    }
-    activePortfolio();
 });
