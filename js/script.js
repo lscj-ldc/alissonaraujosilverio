5954297de3f02c79c9aa115e7963da04ec851eff
Luís da Silveira Carneiro Júnior <lscj.ldc@gmail.com>
2024-12-22 21:15:06 -0300

    Atualização da página sobre

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none'
 }
+
+/* slider show */
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function() {
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function() {
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
